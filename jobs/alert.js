912caccb2fa0e8aa58a9ecd7b7beecc0becc2960
SevenYu <dofyyu@gmail.com>
2015-08-06 16:05:22 +0800

    增加订餐提醒任务

---
+++ jobs/alert.js
@@ -0,0 +1,33 @@
+var $ = require('../lib/utils');
+
+module.exports = {
+    name: 'Alert',
+    time: '0 0 14 * * 1-5',
+    tick: alert
+}
+
+function alert() {
+    $.wechat.getUser(function(err, result) {
+        var openid;
+        if(result.errcode){
+            console.log(result);
+            return;
+        }
+        while(openid = result.data.openid.pop()) {
+            $.wechat.sendByTemplate(openid, $.config.wechat.template.alert, {
+                first: {
+                    value: '订餐提醒：'
+                },
+                schedule: {
+                    value: '每日订餐提醒'
+                },
+                time: {
+                    value: '每天 14:00 提醒一次'
+                },
+                remark: {
+                    value: '别忘了订今天的晚餐哦～'
+                }
+            });
+        }
+    });
+}
